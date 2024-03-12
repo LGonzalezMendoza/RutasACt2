7c822938f12790edbe837b3558d4df033af168d5
LGonzalezMendoza <143547970+LGonzalezMendoza@users.noreply.github.com>
2024-03-12 07:21:03 -0600

    Create Pantalla2-0490.dart

---
+++ lib/Pantalla2-0490.dart
@@ -0,0 +1,18 @@
+//Pantalla2 0490
+import 'package:flutter/material.dart';
+
+class Pantalla2_0490 extends StatelessWidget {
+  const Pantalla2_0490({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("P2 Gonzalez Leonardo  0490 "),
+      ),
+      body: Center(
+        child: Text("Pantalla 2"),
+      ),
+    );
+  }
+}
